ff0fb185ebde3c4516a106cec93c28505c75e9aa
Joshua Harmon <54410912+joshrharmon@users.noreply.github.com>
2020-06-19 08:33:15 -0700

    Create WinManage.h

---
+++ C/BatchDelete/WinManage.h
@@ -0,0 +1,18 @@
+#ifndef WIN_MANAGE_H
+#define WIN_MANAGE_H
+
+typedef struct WIN_STRUCT {
+  int rows, cols, topx, topy;
+} win;
+
+/* Function to quickly create windows and put a box around it */
+WINDOW* initWin(int length, int width, int y, int x);
+
+/* Print utility functions */
+void printStatus(WINDOW *win, char *query, char *path, char *status);
+void printPrompt(WINDOW *win, char *prompt);
+
+/* Function to check for valid input */
+int inputHandler(const char *input, int type);
+
+#endif
