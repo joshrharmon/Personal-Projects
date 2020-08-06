b3353b66866e20d6f914267a779fe7a393896341
Joshua Harmon <54410912+joshrharmon@users.noreply.github.com>
2020-08-05 22:59:31 -0700

    Update WinManage.c
    Minor changes to fix UI elements of program.

--- C/BatchDelete/WinManage.c
+++ C/BatchDelete/WinManage.c
@@ -11,12 +11,14 @@ void printStatus(WINDOW *win, char *query, char *path, char *status) {
     mvwprintw(win, 1, 2, "Path   -> '%s'", path);
     mvwprintw(win, 2, 2, "Query  -> '%s'", query); 
     mvwprintw(win, 3, 2, "Status -> %s", status);  
+	box(win, 0, 0);
 }
 
-void printPrompt(WINDOW *win, char *prompt) {
+void printPrompt(WINDOW *win, char *instr) {
+	box(win, 0, 0);
     wattron(win, A_BOLD | COLOR_PAIR(3));
     mvwprintw(win, 0, 2, "[ Prompt ]");
-    mvwprintw(win, 0, 13, "[ Instruction: %s ]", prompt);
+    mvwprintw(win, 0, 13, "[ Instruction: %s ]", instr);
     wattroff(win, A_BOLD | COLOR_PAIR(3));
 }
 
@@ -26,12 +28,16 @@ WINDOW* initWin(int length, int width, int y, int x) {
     return temp;
 }
 
-void winclear(WINDOW *win) {
-	for(int i = 1; i < win->_maxy; i++) {
-		for(int j = 1; j < win->_maxx; j++) {
-			mvwprintw(win, i, j, " ");
-		}
-	}
+/* 
+    Mode == 0: Clearing an inner window within another window
+    Mode == 1: Clearing a window without a separate, inner window
+*/
+void winclear(WINDOW *win, int mode) {
+    for(int i = mode; i < win->_maxy; i++) {
+        for(int j = mode; j < win->_maxx; j++) {
+            mvwprintw(win, i, j, " ");
+        }
+    }
 }
 
 /* Will handle all error-checking for user input. 1 is valid, 0 is invalid. */
